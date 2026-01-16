fd1c5470010a1439a439abaecb1a55b353acdabd
Amanda Ogletree <amandaogletree19@gmail.com>
2026-01-15 20:57:47 -0500

    Created new book model. Compiled Schema and exported model with name 'Book'.

---
+++ models/Book.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+//Structure Schema with Validation
+const bookSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+  },
+  author: {
+    type: String,
+    required: [true, "Author is required"],
+  },
+  isbn: {
+    type: String,
+    unique: true
+  },
+  publicationDate: Date,
+  inStock: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+//Compile and Export
+const Book = mongoose.model('Book', bookSchema);
+module.exports = Book
